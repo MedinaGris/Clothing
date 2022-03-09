3d064830611b9cfc6381b456dfc76b0456c7617b
Medina <Medina@192.168.1.86>
2022-03-08 22:10:57 -0600

    Creaciòn de mètodos de conexiòn

--- Clothiing/src/java/db/Conexion.java
+++ Clothiing/src/java/db/Conexion.java
@@ -6,41 +6,87 @@
 
 package db;
 
-import jakarta.jms.Connection;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexion {
-    private final Connection connection;
-    private final String url;
-    private final String user;
-    private final String pwd;
+    private final String pass; // = "administrador";
+    private final String user;// = "administrador";
+    private final String url; //= "localhost";
+    private final String nombrebd;
+    private  Connection con = null;
 
-    
-    public Conexion(Connection connection, String url, String user, String pwd) {
-        this.connection = connection;
-        this.url = url;
-        this.user = user;
-        this.pwd = pwd;
+    public Conexion(String user, String pass,String url, String nombrebd ) {
+        this.user= user;
+        this.pass= pass;
+        this.url= url;
+        this.nombrebd= nombrebd;
     }
     
     public void Conectar(){
-        
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.con = DriverManager.getConnection("jdbc:postgresql://" + url + "/" + nombrebd, user, pass);
+            System.out.println("conexion establecida");
+        } catch (Exception ex) {
+            System.out.println("Error al conectar:" + ex.getMessage());
+        }
     }
     
     public void Desconectar(){
+         try {
+            con.close();
+            con=null;
+            System.out.println("conexion finalizada");
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.getMessage());
+        }
         
     }
     
    
     public Connection getConnection() {
-        return connection;
+        
+        return this.con;
     }
 
-    public static void main(String args[]){
-       
-       
+    @Override
+    public String toString() {
+        return "Conexion{" + "pass=" + pass + ", user=" + user + ", url=" + url + ", con=" + con + '}';
     }
     
     
     
-}
+    public static void main(String args[]){
+       Conexion c=new Conexion("administrador","administrador","localhost","ferregra");
+        System.out.println("Datos: "+ c.toString());
+        c.Conectar();
+        c.Desconectar();
+       
+    }
+    
+} 
+    
+
+//
+//    public ResultSet ejecuta(String sql, String bd) {
+//        ResultSet rs = null;
+//        try {
+//            con = conectaBase(bd);
+//            java.sql.Statement st = con.createStatement();
+//            rs = st.executeQuery(sql);
+//        } catch (Exception ex) {
+//            System.out.println("Error:" + ex.getMessage());
+//        }
+//        return rs;
+//    }
+    
+   
+
+
+
+    
+    
+    
